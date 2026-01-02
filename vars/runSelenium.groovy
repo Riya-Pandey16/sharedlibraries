dff0cc28343534aca15e9ac63f9c103e502528d4
Riya-Pandey16 <152271173+Riya-Pandey16@users.noreply.github.com>
2026-01-02 16:51:34 +0530

    Modify Selenium test execution to use JAR file
    Updated the Selenium test execution to run from a JAR file and added a check for the presence of 'testing.jar'.

--- vars/runSelenium.groovy
+++ vars/runSelenium.groovy
@@ -1,6 +1,14 @@
 def call() {
-    echo "Running Selenium Test Suite"
+    echo "Running Selenium Tests from JAR"
+
     dir('FunctionalTesting') {
-        sh "mvn test"
+        sh '''
+            if [ ! -f testing.jar ]; then
+                echo "testing.jar not found!"
+                exit 1
+            fi
+
+            java -jar testing.jar
+        '''
     }
 }
