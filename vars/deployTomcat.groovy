f68b64e245f78134e68b676e7b36b6c71e94c4f5
Riya-Pandey16 <152271173+Riya-Pandey16@users.noreply.github.com>
2026-01-02 11:08:53 +0530

    Update deployTomcat.groovy

--- vars/deployTomcat.groovy
+++ vars/deployTomcat.groovy
@@ -1,4 +1,14 @@
-def deployTomcat(jobname,ip,context)
-{
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+def call(String server, String warName) {
+    sh """
+    echo "Copying WAR to remote server"
+    scp target/${warName} ${server}:/opt/deploy/
+
+    echo "Deploying application"
+    ssh ${server} '
+        sudo systemctl stop tomcat10
+        sudo rm -rf /var/lib/tomcat10/webapps/${warName.replace(".war","")}*
+        sudo mv /opt/deploy/${warName} /var/lib/tomcat10/webapps/
+        sudo systemctl start tomcat10
+    '
+    """
 }
