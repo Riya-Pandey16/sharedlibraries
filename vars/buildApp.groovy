c4e8273126865844b714a7e0ba3656a01ba983eb
Riya-Pandey16 <152271173+Riya-Pandey16@users.noreply.github.com>
2026-01-02 11:39:30 +0530

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,3 +2,17 @@ def call() {
     echo "Building application using Maven"
     sh "mvn package"
 }
+
+stage('Verify WAR') {
+    agent { label 'myslave' }
+    steps {
+        sh '''
+            echo "Workspace:"
+            pwd
+            echo "Listing target directory:"
+            ls -l target || echo "❌ target directory not found"
+        '''
+    }
+}
+
+
