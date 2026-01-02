2a881f754571b5d316a32cf1102927e223b2230c
Riya-Pandey16 <152271173+Riya-Pandey16@users.noreply.github.com>
2026-01-02 11:45:49 +0530

    Modify deployTomcat to use warPattern instead of warName

--- vars/deployTomcat.groovy
+++ vars/deployTomcat.groovy
@@ -1,13 +1,15 @@
-def call(String server, String warName) {
+def call(String server, String warPattern) {
     sh """
     echo "Copying WAR to remote server"
-    scp target/${warName} ${server}:/opt/deploy/
+    ls -l target
+
+    scp -o StrictHostKeyChecking=no target/${warPattern} ${server}:/opt/deploy/
 
     echo "Deploying application"
-    ssh ${server} '
+    ssh -o StrictHostKeyChecking=no ${server} '
         sudo systemctl stop tomcat10
-        sudo rm -rf /var/lib/tomcat10/webapps/${warName.replace(".war","")}*
-        sudo mv /opt/deploy/${warName} /var/lib/tomcat10/webapps/
+        sudo rm -rf /var/lib/tomcat10/webapps/*
+        sudo mv /opt/deploy/*.war /var/lib/tomcat10/webapps/
         sudo systemctl start tomcat10
     '
     """
